0092c72f78994cd32bea869cdb7589f0a07e2cb1
EmmaTosato <emmamariasole.tosato@studenti.unipd.it>
2025-05-15 11:28:09 +0200

    changed networks

--- src/cnn/models.py
+++ src/cnn/models.py
@@ -2,7 +2,8 @@
 import torch
 import torch.nn as nn
 from torchvision.models.video import r3d_18
-
+from monai.networks.nets import DenseNet121
+from medmamba.medmamba import Med_Mamba_tiny
 
 class ResNet3D(nn.Module):
     def __init__(self, n_classes):
@@ -15,82 +16,15 @@ class ResNet3D(nn.Module):
         return self.model(x)
 
 
-class VGG3D(nn.Module):
+class DenseNet3D(nn.Module):
     def __init__(self, n_classes):
-        super(VGG3D, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv3d(1, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=2, stride=2),
-
-            nn.Conv3d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=2, stride=2),
-
-            nn.Conv3d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=2, stride=2),
-
-            nn.Conv3d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.AdaptiveAvgPool3d(1)
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(512, 256),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-            nn.Linear(256, n_classes)
+        super(DenseNet3D, self).__init__()
+        self.model = DenseNet121(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=n_classes,
+            pretrained=False
         )
 
     def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-
-
-class AlexNet3D(nn.Module):
-    def __init__(self, n_classes):
-        super(AlexNet3D, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv3d(1, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=3, stride=2),
-
-            nn.Conv3d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=3, stride=2),
-
-            nn.Conv3d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=3, stride=2),
-
-            nn.AdaptiveAvgPool3d(1)
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256, 128),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(128, n_classes)
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
+        return self.model(x)
